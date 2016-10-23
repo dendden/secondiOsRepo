5afabf5a6128ab47384549b3e2d83f69ff26d9df
Denys Triasunov <dendden@users.noreply.github.com>
2016-10-23 11:48:33 +0300

    Initial Commit

---
+++ secondiOsRepo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  secondiOsRepo
+//
+//  Created by Денис Трясунов on 10/23/16.
+//  Copyright © 2016 Денис Трясунов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
